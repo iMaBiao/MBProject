3c8dd9bc3de0d91d1be06f8cc4ef9a42cc59a579
MaBiao <>
2018-04-12 09:06:47 +0800

    first commit

---
+++ MBProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MBProject
+//
+//  Created by MaBiao on 2018/4/12.
+//  Copyright © 2018年 MaBiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MBProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MBProject
+//
+//  Created by MaBiao on 2018/4/12.
+//  Copyright © 2018年 MaBiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
